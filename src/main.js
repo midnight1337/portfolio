8bfcf2a5bc99c4edc345b6b4b33ab2ed4213d59e
midnight1337 <kamil.koltows@gmail.com>
2024-04-20 15:21:43 +0200

    Blinking cursor fix, some other changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
-import '../styles/components/hero.css'
 import '../styles/components/about.css'
 import '../styles/components/featured.css'
 import '../styles/components/contact.css'
