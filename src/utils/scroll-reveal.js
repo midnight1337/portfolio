8f92f5eb2898c7ec66a67a15c3b4ed17f6ff88fc
midnight1337 <kamil.koltows@gmail.com>
2024-04-22 11:20:58 +0200

    work section, content, responsive, content scroll reveals

--- src/utils/scroll-reveal.js
+++ src/utils/scroll-reveal.js
@@ -8,6 +8,8 @@ const scroll_reveal = () => {
 
     sr.reveal('.about__content', { origin: 'left' });
     sr.reveal('.about__img-wrapper', { origin: 'top' });
+    sr.reveal('.experience__description', { origin: 'left' });
+    sr.reveal('.experience__responsibilites-wrapper', { origin: 'right' });
 }
 
 export default scroll_reveal;
